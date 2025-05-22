1276f4a8f8fdde3c11218f96da5e4f87ae7faa72
Kaiquecarvalho08 <kaiquecarvalho57272@gmail.com>
2025-05-22 11:16:16 -0300

    Aqui eu vi let,console, e fiz uma calculadora

--- logicacomjs.js
+++ logicacomjs.js
@@ -1,1 +1,20 @@
-console.log("oi")
+let number1 = 60;
+let number2 = 40;
+let som = number1+number2
+console.log("A soma é: "+som)
+console.log("-------------------")
+let number3= 60;
+let number4 = 40;
+let subtração = number3-number4
+console.log("A subtração é: "+subtração)
+console.log("-------------------")
+let number5= 6;
+let number6 = 6;
+let multiplicação = number5*number6
+console.log("A multiplicação é: "+multiplicação)
+console.log("-------------------")
+let number7= 100;
+let number8= 2;
+let divisão = number7/number8
+console.log("A divisão é: "+divisão)
+console.log("-------------------")
